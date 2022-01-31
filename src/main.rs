8d7900c9382d4427661986a673f5f5006cd18053
worldmaker <worldmaker18349276@gmail.com>
2022-01-31 12:57:17 +0800

    define some structs in model

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod model {
+    #[derive(Debug, Copy, Clone)]
+    enum Chess {
+        Pawn,
+        Drone,
+        Queen,
+    }
+
+    #[derive(Debug, Copy, Clone)]
+    enum Player {
+        Player1,
+        Player2,
+    }
+
+    #[derive(Debug, Copy, Clone)]
+    struct Playfield {
+        board : [[Option<Chess>; 4]; 8],
+        scores : [i32; 2],
+        turn : Player,
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
